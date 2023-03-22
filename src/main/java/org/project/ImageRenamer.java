9adf19beecdf39d9fc8a5287bd9537d6bc842c3b
Mustafa Arifoglu <mustafarifoglu@gmail.com>
2023-03-22 15:44:05 +0000

    Duplicated images issues fixed

--- src/main/java/org/project/ImageRenamer.java
+++ src/main/java/org/project/ImageRenamer.java
@@ -1,16 +1,36 @@
 package org.project;
 
 import java.io.File;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import com.drew.imaging.ImageMetadataReader;
 import com.drew.imaging.ImageProcessingException;
 import com.drew.metadata.Metadata;
 import com.drew.metadata.exif.ExifIFD0Directory;
 
 public class ImageRenamer {
+
+    private static final Logger logger = LoggerFactory.getLogger(ImageRenamer.class);
+
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            logger.error("Please provide a valid directory path.");
+            return;
+        }
+
+        String directoryPath = args[0];
+        File directory = new File(directoryPath);
+        if (!directory.exists() || !directory.isDirectory()) {
+            logger.error("The specified directory does not exist or is not a directory.");
+            return;
+        }
+
+        renameImages(directory);
+    }
 
     public static void renameImages(File directory) {
         File[] files = directory.listFiles();
@@ -31,18 +51,19 @@ public class ImageRenamer {
                     extension = fileName.substring(i + 1).toLowerCase();
                 }
 
-                if (extension.equals("jpg") || extension.equals("jpeg") || extension.equals("png") || extension.equals("mov")) {
+                if (isSupportedFileType(extension)) {
                     try {
                         Metadata metadata = ImageMetadataReader.readMetadata(file);
+                        Date date = null;
 
                         ExifIFD0Directory exifDirectory = metadata.getFirstDirectoryOfType(ExifIFD0Directory.class);
-                        Date date = null;
                         if (exifDirectory != null) {
                             date = exifDirectory.getDate(ExifIFD0Directory.TAG_DATETIME_ORIGINAL);
                             if (date == null) {
                                 date = exifDirectory.getDate(ExifIFD0Directory.TAG_DATETIME);
                             }
                         }
+
                         if (date == null) {
                             date = new Date(file.lastModified());
                         }
@@ -53,18 +74,56 @@ public class ImageRenamer {
 
                         File newFile = new File(file.getParent(), newFileName);
 
-                        if (!newFile.exists()) {
-                            file.renameTo(newFile);
+                        int count = 0;
+                        while (newFile.exists()) {
+                            count++;
+                            newFileName = dateString + "_" + count + "." + extension;
+                            newFile = new File(file.getParent(), newFileName);
+                        }
+
+                        if (file.renameTo(newFile)) {
+                            logger.info("Renamed file: {}", newFile.getAbsolutePath());
+                        } else {
+                            logger.error("Failed to rename file: {}", file.getAbsolutePath());
                         }
                     } catch (ImageProcessingException e) {
-                        System.err.println("Error processing " + file.getAbsolutePath() + ": " + e.getMessage());
+                        logger.error("Error processing file: {}", file.getAbsolutePath(), e);
+                    } catch (IOException e) {
+                        logger.error("IO error occurred while processing file: {}", file.getAbsolutePath(), e);
                     } catch (Exception e) {
-                        System.err.println("Error renaming " + file.getAbsolutePath() + ": " + e.getMessage());
+                        logger.error("Unexpected error occurred while processing file: {}", file.getAbsolutePath(), e);
                     }
                 }
             }
         }
     }
+    private static boolean isSupportedFileType(String extension) {
+        return SupportedFileType.contains(extension);
+    }
+
+    private enum SupportedFileType {
+        JPG("jpg"),
+        JPEG("jpeg"),
+        PNG("png"),
+        MOV("mov");
+
+        private final String extension;
+
+        SupportedFileType(String extension) {
+            this.extension = extension;
+        }
+
+        public static boolean contains(String extension) {
+            for (SupportedFileType fileType : SupportedFileType.values()) {
+                if (fileType.extension.equalsIgnoreCase(extension)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public String getExtension() {
+            return extension;
+        }
+    }
 }
-
-
--- src/main/java/org/project/Main.java
+++ src/main/java/org/project/Main.java
@@ -2,11 +2,15 @@ package org.project;
 
 
 import java.io.File;
+import java.io.IOException;
+
 import static org.project.ImageRenamer.*;
 
+
+        // File directory = new File("C:/Users/userName/Desktop/files");
 public class Main {
-    public static void main(String[] args) {
-        File directory = new File("C:/Users/userName/Desktop/files");
+    public static void main(String[] args) throws IOException {
+        File directory = new File("C:/Users/musta/OneDrive/Desktop/test/roll");
         renameImages(directory);
     }
 }
